1205927edd85e9e19f21cd2f39730503b2984958
songjiannan <song_jiannan@hoperun.com>
2019-10-23 12:35:13 -0400

    support ota feature.

--- targets/STM32F429IGTx_FIRE/Src/ota_adaptor.c
+++ targets/STM32F429IGTx_FIRE/Src/ota_adaptor.c
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------
- * Copyright (c) <2018>, <Huawei Technologies Co., Ltd>
+ * Copyright (c) <2016-2018>, <Huawei Technologies Co., Ltd>
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -31,14 +31,81 @@
  * Import, export and usage of Huawei LiteOS in any manner by you shall be in compliance with such
  * applicable export control laws and regulations.
  *---------------------------------------------------------------------------*/
-/**
- *  DATE                AUTHOR      INSTRUCTION
- *  2019-05-28 09:48  zhangqianfu  The first version  
- *
- */
+#include "ota_adaptor.h"
+#include "ota_flag.h"
 
-/**
- * @brief: this is the loader main file, if you need the loader, you should compile this
- *         this file  and run this code first
- * */
+static int ota_flag_read(ota_flag_t *flag)
+{
+  if (flag != NULL) {
+    printf("SPI FLAG R\n");
+    return hal_spi_flash_read((void *)flag, sizeof(ota_flag_t), OTA_FLAG_ADDR1);
+  }
+  return -1;
+}
+
+static int ota_flag_write(ota_flag_t *flag)
+{
+  if (flag != NULL) {
+    printf("SPI FLAG W:state %d\n", flag->cur_state);
+    return flash_adaptor_write(OTA_FLAG_ADDR1, (void *)flag, sizeof(ota_flag_t));
+  }
+  return -1;
+}
+
+static int ota_bin_read(uint32_t offset, void *buf, int len)
+{
+  if (buf != NULL) {
+    printf("SPI BIN R: %08d %08d\n", offset, len);
+    return hal_spi_flash_read(buf, len, offset + OTA_IMAGE_DOWNLOAD_ADDR);
+  }
+  return -1;
+}
+
+static int ota_bin_write(uint32_t offset, void *buf, int len)
+{
+  if (buf != NULL) {
+    printf("SPI BIN W: %08d %08d\n", offset, len);
+    return flash_adaptor_write(offset + OTA_IMAGE_DOWNLOAD_ADDR, buf, len);
+  }
+  return -1;
+}
+
+static ota_storage_t  prv_ota_flag_s =
+{
+   .name = "fire_ota_spi_storage",
+   {
+    .flag_read = ota_flag_read,
+    .flag_write = ota_flag_write,
+    .bin_read = ota_bin_read,
+    .bin_write = ota_bin_write,
+   }
+};
+
+/* 0: full upgrade, 1: diff upgrade */
+EN_PACKAGE_TYPE get_package_type(const uint8_t *data, uint32_t len)
+{
+  ota_pack_info *info;
+  if (data == NULL ||len <= sizeof(ota_pack_info))
+    return -1;
+
+  info = (ota_pack_info *)data;
+  return info->pack_type;
+}
+
+void ota_update_upgrade_result(ota_flag_t *flag, uint32_t result)
+{
+  if (flag != NULL) {
+    flag->cur_state = EN_OTA_STATUS_UPGRADED;
+    flag->ret_upgrade = result;
+    flag->crc = calc_crc32(0, flag, sizeof(*flag) - sizeof(flag->crc));
+    ota_storage_flag_write(flag);
+  }
+}
+
+void hal_init_ota(void)
+{
+  printf("HAL OTA init\n");
+  flash_adaptor_init();
+  ota_storage_install(&prv_ota_flag_s);
+}
 
