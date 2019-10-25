f64eb28fd31c5708a28ef28bf68416d2da09a854
xuliqun25 <xu_liqun@hoperun.com>
2019-10-25 19:55:20 +0800

    oc_atiny_mqtt msgdealer cannot be null

--- iot_link/oc/oc_mqtt/atiny_mqtt/atiny_mqtt.c
+++ iot_link/oc/oc_mqtt/atiny_mqtt/atiny_mqtt.c
@@ -131,12 +131,12 @@ static int check_clone_config_params(oc_agent_mqtt_cb_t *cb,tag_oc_mqtt_config *
         goto EXIT_ERR;
     }
 
-    if(en_oc_mqtt_code_mode_json != config->code_mode )
+    if(en_oc_mqtt_code_mode_json != config->code_mode)
     {
         goto EXIT_ERR;
     }
 
-    if(en_mqtt_sign_type_hmacsha256_check_time_no != config->sign_type )
+    if(en_mqtt_sign_type_hmacsha256_check_time_no != config->sign_type)
     {
         goto EXIT_ERR;
     }
@@ -169,6 +169,11 @@ static int check_clone_config_params(oc_agent_mqtt_cb_t *cb,tag_oc_mqtt_config *
 
     if((en_oc_mqtt_device_type_dynamic == config->device_type) &&\
         (en_mqtt_auth_type_model != config->auth_type))
+    {
+        goto EXIT_ERR;
+    }
+	
+    if(NULL == config->msgdealer)
     {
         goto EXIT_ERR;
     }
