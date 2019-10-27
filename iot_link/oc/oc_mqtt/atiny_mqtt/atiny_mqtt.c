121f9978a8a04403905693d35ce998110b845c30
xuliqun25 <xu_liqun@hoperun.com>
2019-10-27 13:00:29 +0800

    del blank line

--- iot_link/oc/oc_mqtt/atiny_mqtt/atiny_mqtt.c
+++ iot_link/oc/oc_mqtt/atiny_mqtt/atiny_mqtt.c
@@ -172,7 +172,6 @@ static int check_clone_config_params(oc_agent_mqtt_cb_t *cb,tag_oc_mqtt_config *
     {
         goto EXIT_ERR;
     }
-	
 
     memcpy(&cb->config,config,sizeof(tag_oc_mqtt_config));
 
