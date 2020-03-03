3bb603e36b76feac5fa0a30d2734c2230173c4d1
chaifangming <fangming.chai@huawei.com>
2020-03-03 16:34:17 +0800

    updated service, now all services share one task

--- iot_link/service/service.h
+++ iot_link/service/service.h
@@ -47,8 +47,7 @@ typedef uintptr_t               service_id;
 
 /* externs */
 
-extern service_id service_create  (const char *name, int (*handler) (void *),
-                                   int stack_size, int prio);
+extern service_id service_create  (const char *name, int (*handler) (void *), int prio);
 extern service_id service_open    (const char *name);
 extern bool_t     service_close   (service_id sid);
 extern bool_t     service_send    (service_id sid, void *msg, void (*pfn) (void *, int));
