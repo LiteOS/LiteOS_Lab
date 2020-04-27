438def3d1ba8a4ccae439364a0b573f18244118f
bearpi <admin@bearpi.cn>
2020-04-27 22:32:07 +0800

    modify OS func demo defaults.sdkconfig

--- targets/STM32L431_BearPi_OS_Func/Demos/user_demo.mk
+++ targets/STM32L431_BearPi_OS_Func/Demos/user_demo.mk
@@ -36,10 +36,10 @@
 		user_demo_src  = ${wildcard $(TARGET_DIR)/Demos/osal_mutex_demo/*.c}
 		user_demo_defs = -D CONFIG_OSAL_MUTEX_ENABLE=1
 	endif
-	#example for os_interrupt_demo 
-	ifeq ($(CONFIG_USER_DEMO), "os_interrupt_demo")	
-		user_demo_src  = ${wildcard $(TARGET_DIR)/Demos/os_interrupt_demo/*.c}
-		user_demo_defs = -D CONFIG_INTERRUPT_ENABLE=1
+	#example for osal_interrupt_demo 
+	ifeq ($(CONFIG_USER_DEMO), "osal_interrupt_demo")	
+		user_demo_src  = ${wildcard $(TARGET_DIR)/Demos/osal_interrupt_demo/*.c}
+		user_demo_defs = -D CONFIG_OSAL_INTERRUPT_ENABLE=1
 	endif
 	
 	C_SOURCES += $(user_demo_src)
