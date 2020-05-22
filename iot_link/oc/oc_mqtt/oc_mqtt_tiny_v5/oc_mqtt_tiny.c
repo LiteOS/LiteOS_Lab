c44616759afe57a4a82882b95ff54e3abbc4dae1
zhangqianfu@huawei.com <zhangqianfu@huawei.com>
2020-05-22 21:19:08 +0800

    fix:paho:changge the resource release in the task fix:oc_mqtt:fix the subscribe selfdefined topic function

--- iot_link/oc/oc_mqtt/oc_mqtt_tiny_v5/oc_mqtt_tiny.c
+++ iot_link/oc/oc_mqtt/oc_mqtt_tiny_v5/oc_mqtt_tiny.c
@@ -935,9 +935,9 @@ static int deal_api_subscribe( oc_mqtt_tiny_cb_t  *cb, oc_mqtt_daemon_cmd_t *cmd
     int ret = (int)en_oc_mqtt_err_noconfigured;
     tiny_topic_sub_t  *topic_sub;
     mqtt_al_subpara_t *subpara;
+    mqtt_al_subpara_t  sub_new;
 
     subpara = cmd->arg;
-
     if((int)en_daemon_status_idle == cb->flag.bits.bit_daemon_status)
     {
         ret = (int)en_oc_mqtt_err_noconfigured;
@@ -949,19 +949,24 @@ static int deal_api_subscribe( oc_mqtt_tiny_cb_t  *cb, oc_mqtt_daemon_cmd_t *cmd
         topic_sub = osal_malloc( sizeof( tiny_topic_sub_t ) + subpara->topic.len + 1 );
         if ( NULL != topic_sub)
         {
+            ///< initialize the subtopic add it to the subscribe list
+            topic_sub->qos = (int) subpara->qos;
+            topic_sub->topic = (char *)topic_sub + sizeof(tiny_topic_sub_t);
+            (void) strncpy(topic_sub->topic, subpara->topic.data,subpara->topic.len);
+            topic_sub->topic[subpara->topic.len] = '\0';
 
-            subpara->arg = cb;
-            subpara->dealer = hub_msg_default_deal;
-            subpara->timeout = CN_OC_MQTT_TIMEOUT;
-            if( 0  == mqtt_al_subscribe( cb->mqtt_para.mqtt_handle, subpara) )
+            ///< copy the old parameters to the new one, for we could not change the user'd memory
+            (void)memset(&sub_new,0,sizeof(sub_new));
+            sub_new.arg = cb;
+            sub_new.dealer = hub_msg_default_deal;
+            sub_new.timeout = CN_OC_MQTT_TIMEOUT;
+            sub_new.qos = topic_sub->qos;
+            sub_new.topic.data  = topic_sub->topic;
+            sub_new.topic.len = subpara->topic.len;
+            if( 0  == mqtt_al_subscribe( cb->mqtt_para.mqtt_handle, &sub_new) )
             {
-                ///< initialize the subtopic add it to the subscribe list
-                topic_sub->qos = (int) subpara->qos;
-                topic_sub->topic = (char *)topic_sub + sizeof(tiny_topic_sub_t);
-                (void) strncpy(topic_sub->topic, subpara->topic.data,strlen(subpara->topic.data)+1);
                 topic_sub->nxt = cb->subscribe_lst;
                 cb->subscribe_lst = topic_sub;
-
                 ret = (int)en_oc_mqtt_err_ok;
             }
             else
@@ -969,6 +974,7 @@ static int deal_api_subscribe( oc_mqtt_tiny_cb_t  *cb, oc_mqtt_daemon_cmd_t *cmd
                 osal_free ( topic_sub );
                 ret = (int)en_oc_mqtt_err_subscribe;
             }
+            subpara->subret = sub_new.subret;
         }
         else
         {
